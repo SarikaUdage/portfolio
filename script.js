4539ab325c6db711fb4a5c54f649da363f829cb8
sarikaudage <sarikaudage2006@gmail.com>
2025-05-09 22:52:40 +0530

    created responssive page

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-
 function showContent(id) {
     const contents = document.querySelectorAll('.content-toggle');
-    contents.forEach(content => {
-        content.classList.remove('active');
-    });
-
-    const selected = document.getElementById(id);
-    if (selected) {
-        selected.classList.add('active');
-    }
+    contents.forEach(content => content.classList.remove('active'));
+    document.getElementById(id).classList.add('active');
 }
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('navLinks');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+});
+document.querySelectorAll('#navLinks a').forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.classList.remove('show');
+    });
+});
 
